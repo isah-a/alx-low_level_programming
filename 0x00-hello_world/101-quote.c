6f7d98c9203a242f2aa5ece4eb0c862d50c355f3
Isah <isahabdul16@gmail.com>
2023-06-23 03:23:11 -0700

    up

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,12 +2,16 @@
 #include <unistd.h>
 
 /**
- * main - Entry point
- * Return: Always 1 (Success)
+ * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ *                followed by a new line, to standard error.
+ *
+ * Return: Always 1.
  */
 int main(void)
 {
-	fput( "and that piece of art is useful\" - Dora Korpar, 
-			2015-10-19\n", stdout);
+	write(2,
+	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	      59);
+
 	return (1);
 }
