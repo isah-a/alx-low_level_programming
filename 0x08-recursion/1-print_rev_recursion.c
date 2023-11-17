5af83d3dea2611b4a0e2a58ff3ae02aa68ac52ad
Isah <isahabdul16@gmail.com>
2023-11-17 11:37:47 +0100

    fixed bug

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -8,10 +8,11 @@ void _print_rev_recursion(char *s)
 {
 	unsigned int i;
 
-	if (s[i] == "\0")
+	if (s[i] == '\0')
 	{
-		_putchar("\n");
+		_putchar('\n');
 	}
+	_putchar(s[i]);
 	_print_rev_recursion(s + i + 1);
 	i++;
 }
